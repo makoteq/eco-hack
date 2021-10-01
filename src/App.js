a1bc781aeb35a7da122db9c64ded31055b366ef8
Maciej Kotowski <makoteq@gmail.com>
2021-10-01 22:56:52 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import styles from "./App.module.scss";
-
-function App() {
-    return <div></div>;
-}
-
-export default App;
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+  export default function App() {
+    return (
+      <Router>
+        <div>
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+  
+  function Home() {
+    return <h2>Home</h2>;
+  }
+  
+  function About() {
+    return <h2>About</h2>;
+  }
+  
+  function Users() {
+    return <h2>Users</h2>;
+  }
