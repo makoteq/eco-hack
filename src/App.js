0b73bd1befde3b92fae9b38962fd7ff45cde5d1f
drgoodcat <60068941+drgoodcat@users.noreply.github.com>
2021-10-02 23:13:06 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import { BrowserRouter as Router, Switch, Route /*Link*/ } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route /*Link*/,
+} from "react-router-dom";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Users } from "./pages/Users";
@@ -6,15 +10,17 @@ import { NotFound } from "./pages/NotFound";
 import Logo from "./components/Logo";
 
 export const App = () => {
-    return (
-        <Router>
-            <Logo />
-            <Switch>
-                <Route exact path="/about" component={About} />
-                <Route exact path="/users" component={Users} />
-                <Route exact path="/" component={Home} />
-                <Route component={NotFound} />
-            </Switch>
-        </Router>
-    );
+  return (
+    <Router>
+      <Logo />
+      <Switch>
+        <Route exact path="/about" component={About} />
+        <Route exact path="/users" component={Users} />
+        <Route exact path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
 };
+
+export default App;
