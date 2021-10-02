b7f66a744792dd5a05b7b5b7685998fa6467db15
Maciej Kotowski <makoteq@gmail.com>
2021-10-02 17:07:56 +0200

    add logo to root

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import { BrowserRouter as Router, Switch, Route /*Link*/ } from "react-router-do
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Users } from "./pages/Users";
-
+import Logo from "./components/Logo";
 export const App = () => {
     return (
         <Router>
+          <Logo/>
             <Switch>
                 <Route path="/about" component={About} />
                 <Route path="/users" component={Users} />
