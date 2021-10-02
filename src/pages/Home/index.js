294ebcca929efaac412c886d63c1acb7b3c21db1
Maciej Kotowski <makoteq@gmail.com>
2021-10-02 17:42:56 +0200

    list rendering

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,13 +2,15 @@ import { Stack } from "react-bootstrap";
 import styles from "./index.module.scss";
 
 export const Home = () => {
+    const data =[{"name":"test1"},{"name":"test2"}];
     return (
         <div className={styles.con}>
             {" "}
             <Stack gap={1}>
-                <div className={styles.item}>First item</div>
-                <div className={styles.item}>Second item</div>
-                <div className={styles.item}>Third item</div>
+            {data.map(function(d, idx){
+         return (<div key={idx} className={styles.item}>{d.name}</div>)
+       })}
+                
             </Stack>
         </div>
     );
