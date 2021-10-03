4b1ecdf9a217ab49188bdb2ffe33055ab411def2
GRZ4NA <grzanatv@gmail.com>
2021-10-03 16:03:57 +0200

    removed old getEvents utility function

--- src/utils/api/getEvents.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-import { BACKEND_URL } from "../../constants";
-
-export const getEvents = async () => {
-    const rq = await axios.get(`${BACKEND_URL}/api/getEvents`);
-    if (rq.status !== 200) throw new Error(`Request failed with code ${rq.status}: ${rq.statusText}`);
-    return rq.data;
-};
