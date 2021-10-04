d0616ccecf455b927a5fa2fcb51e7deae41f8d5f
GRZ4NA <grzanatv@gmail.com>
2021-10-04 21:51:55 +0200

    typo fix

--- models/events.js
+++ models/events.js
@@ -2,36 +2,36 @@ const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 
 const eventSchema = new schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  adress: {
-    type: String,
-    required: true,
-  },
-  created:{
-  time:{
-      type:Number,
-      required: false,
+    name: {
+        type: String,
+        required: true,
     },
-  },
-  type: {
-    type: Number,
-    required: false,
-  },
-  time: {
-    type: Number,
-    required: true,
-  },
-  lat: {
-    type: Number,
-    required: false,
-  },
-  lon: {
-    type: Number,
-    required: false,
-  },
+    address: {
+        type: String,
+        required: true,
+    },
+    created: {
+        time: {
+            type: Number,
+            required: false,
+        },
+    },
+    type: {
+        type: Number,
+        required: false,
+    },
+    time: {
+        type: Number,
+        required: true,
+    },
+    lat: {
+        type: Number,
+        required: false,
+    },
+    lon: {
+        type: Number,
+        required: false,
+    },
 });
 
 module.exports = mongoose.model("events", eventSchema);
