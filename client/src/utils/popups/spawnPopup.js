6bb536e51c5454c9535549f90a3995e2c69390fa
GRZ4NA <grzanatv@gmail.com>
2021-10-05 00:24:51 +0200

    Revert "closePopup"
    This reverts commit 0bbb3760a4f62032095ccdde094d5b6bbf1cdcee.

--- client/src/utils/popups/spawnPopup.js
+++ client/src/utils/popups/spawnPopup.js
@@ -9,7 +9,6 @@ export const spawnPopup = async (elements, windowStyles) => {
 
     return new Promise((resolve, reject) => {
         const closeWindow = (value) => {
-            if (container.innerHTML === "") reject("Popup closed using closePopup function");
             container.animate([{ backgroundColor: backgroundColor }, { backgroundColor: "rgba(0,0,0,0)" }], {
                 easing: "ease",
                 duration: animationDuration,
