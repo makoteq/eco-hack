5f99aa8381c12b1a3e41e6655e49a81079489f47
Maciej Kotowski <makoteq@gmail.com>
2021-10-04 18:48:36 +0200

    try to fix

--- client/src/constants.js
+++ client/src/constants.js
@@ -3,4 +3,5 @@ import { APIClient } from "./utils/APIClient";
 require('dotenv').config();
 export const BACKEND_URL = "http://localhost:5000";
 export const EVENT_CONTEXT = createContext();
-export const API_CLIENT = new APIClient(process.env.DEPLOY ? "/" : BACKEND_URL);
+//export const API_CLIENT = new APIClient(process.env.DEPLOY ? "/" : BACKEND_URL);
+export const API_CLIENT = new APIClient("");
