3822e7c9684b5ad6af35b0a76ba8442c9b0b60c7
Maciej Kotowski <makoteq@gmail.com>
2021-10-04 18:56:04 +0200

    comments

--- client/src/constants.js
+++ client/src/constants.js
@@ -4,4 +4,4 @@ require('dotenv').config();
 export const BACKEND_URL = "http://localhost:5000";
 export const EVENT_CONTEXT = createContext();
 export const API_CLIENT = new APIClient(process.env.DEPLOY ? "" : BACKEND_URL);
-//jak robisz deploy to daj na true
+//jak robisz deploy to daj na true, a potem git push heroku main
