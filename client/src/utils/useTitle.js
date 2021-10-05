8e615eb05affff79e5c306342275a72240325df1
GRZ4NA <grzanatv@gmail.com>
2021-10-05 17:07:37 +0200

    removed warning

--- client/src/utils/useTitle.js
+++ client/src/utils/useTitle.js
@@ -4,5 +4,5 @@ import { APP_NAME } from "../constants";
 export const useTitle = (title) => {
     useEffect(() => {
         document.title = title ? `${title} | ${APP_NAME}` : `${APP_NAME}`;
-    }, []);
+    }, [title]);
 };
