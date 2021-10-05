14ab8de7895fb783b6e515c55b2739844a83553f
GRZ4NA <grzanatv@gmail.com>
2021-10-05 17:55:11 +0200

    added description

--- index.js
+++ index.js
@@ -9,56 +9,57 @@ const events = require("./models/events");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); // to support JSON-encoded bodies
 app.use(
-  bodyParser.urlencoded({
-    // to support URL-encoded bodies
-    extended: true,
-  })
+    bodyParser.urlencoded({
+        // to support URL-encoded bodies
+        extended: true,
+    })
 );
 app.use(express.static(path.join(__dirname, "client/build")));
 app.use(
-  cors({
-    origin: "*",
-  })
+    cors({
+        origin: "*",
+    })
 );
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("connected"))
-  .catch((err) => console.log(err));
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("connected"))
+    .catch((err) => console.log(err));
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 app.get("/api/getEvents", async (req, res) => {
-  try {
-    const event = await events.find();
-    if (!event) throw Error("something went wrong");
-    console.log(event);
-    res.status(200).json(event);
-  } catch (err) {
-    res.status(400).json({ msg: err });
-  }
+    try {
+        const event = await events.find();
+        if (!event) throw Error("something went wrong");
+        console.log(event);
+        res.status(200).json(event);
+    } catch (err) {
+        res.status(400).json({ msg: err });
+    }
 });
 app.post("/api/createEvent", (req, res) => {
-  let data = {
-    name: req.body.name,
-    type: req.body.type,
-    address: req.body.address,
-    time: req.body.time,
-    created: { time: Date.now() },
-    lat: req.body.lat,
-    lon: req.body.lon,
-  };
-  events.create(data, (err) => {
-    if (err) return res.status(400).json({ msg: err });
-    // saved!
-    res.status(200).json("ok");
-  });
+    let data = {
+        name: req.body.name,
+        type: req.body.type,
+        description: req.body.description,
+        address: req.body.address,
+        time: req.body.time,
+        created: { time: Date.now() },
+        lat: req.body.lat,
+        lon: req.body.lon,
+    };
+    events.create(data, (err) => {
+        if (err) return res.status(400).json({ msg: err });
+        // saved!
+        res.status(200).json("ok");
+    });
 });
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
