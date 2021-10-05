d645b7c7a5e31a357754ee1d8d1dfc554f03c837
GRZ4NA <grzanatv@gmail.com>
2021-10-06 01:12:58 +0200

    working distance sorting

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -7,8 +7,10 @@ import styles from "./index.module.scss";
 import { BIcon } from "../../components/BIcon";
 // import HoldingPlant from "../../components/Pop-up/AnimatedHand";
 import { API_CLIENT } from "../../constants";
-// import { calculateDistance } from "../../utils/map/calculateDistance";
+import { calculateDistance } from "../../utils/map/calculateDistance";
 import { useHistory } from "react-router";
+import { spawnPopup } from "../../utils/popups/spawnPopup";
+import { spawnError } from "../../utils/popups/spawnError";
 
 export const Home = () => {
     useTitle();
@@ -45,10 +47,30 @@ export const Home = () => {
             );
         });
 
-        sortDropdown.current.addEventListener("input", (e) => {
+        sortDropdown.current.addEventListener("input", async (e) => {
             const value = e.target.value;
             const type = value.split(":")[0];
             const direction = value.split(":")[1];
+            let userPos = [null, null];
+            if (type === "distance") {
+                userPos = await spawnPopup((c) => {
+                    window.navigator.geolocation.getCurrentPosition(
+                        (pos) => {
+                            c([pos.coords.longitude, pos.coords.latitude]);
+                        },
+                        async (e) => {
+                            console.error(e);
+                            await spawnError("Brak dostępu do lokalizacji");
+                            c([null, null]);
+                        }
+                    );
+                    return <h1>Oczekiwanie na przyznanie uprawnień</h1>;
+                });
+                if (userPos === [null, null]) {
+                    sortDropdown.current.value = "event-date:ascending";
+                    return;
+                }
+            }
             const newOrder = API_CLIENT.events.sort((a, b) => {
                 switch (type) {
                     case "event-date":
@@ -58,7 +80,11 @@ export const Home = () => {
                     case "alphabetic":
                         return direction === "ascending" ? (a.name > b.name ? -1 : 1) : a.name > b.name ? 1 : -1;
                     case "distance":
-                        return 0;
+                        if (userPos !== [null, null]) {
+                            const aDistance = a.lon && a.lat ? calculateDistance(userPos, [a.lon, a.lat]) : -1;
+                            const bDistance = b.lon && b.lat ? calculateDistance(userPos, [b.lon, b.lat]) : -1;
+                            return direction === "ascending" ? aDistance - bDistance : bDistance - aDistance;
+                        } else return 0;
                     default:
                         return 0;
                 }
@@ -95,8 +121,8 @@ export const Home = () => {
                         <option value="creation-date:descending">Data utworzenia: od najstarszych</option>
                         <option value="alphabetic:ascending">Alfabetycznie: A-Z</option>
                         <option value="alphabetic:descending">Alfabetycznie: Z-A</option>
-                        <option value="distance:ascending">Odległość: najbliżej [NIE DZIAŁA]</option>
-                        <option value="distance:descending">Odległość: najdalej [NIE DZIAŁA]</option>
+                        <option value="distance:ascending">Odległość: najbliżej</option>
+                        <option value="distance:descending">Odległość: najdalej</option>
                     </select>
                     <button
                         onClick={() => {
