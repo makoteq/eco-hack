a03e53aba37bb84d25b19cebd3f9acb4ba2616f4
GRZ4NA <grzanatv@gmail.com>
2021-10-05 16:59:37 +0200

    create useTitle hook

---
+++ client/src/utils/useTitle.js
@@ -0,0 +1,8 @@
+import { useEffect } from "react";
+import { APP_NAME } from "../constants";
+
+export const useTitle = (title) => {
+    useEffect(() => {
+        document.title = title ? `${title} | ${APP_NAME}` : `${APP_NAME}`;
+    }, []);
+};
