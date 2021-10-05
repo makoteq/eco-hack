becdf573dcb916b5e16268c1503bcbebbb6d53a9
GRZ4NA <grzanatv@gmail.com>
2021-10-06 01:12:43 +0200

    tweaked to return an absolute value

--- client/src/utils/map/calculateDistance.js
+++ client/src/utils/map/calculateDistance.js
@@ -4,5 +4,5 @@ export const calculateDistance = (posA, posB) => {
     if (!Array.isArray(posA) || !Array.isArray(posB) || isNaN(posA[0]) || isNaN(posA[1]) || isNaN(posB[0]) || isNaN(posB[1])) throw new TypeError("Incorrect argument type");
     const distanceX = posA[0] - posB[0];
     const distanceY = posA[1] - posB[1];
-    return Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2));
+    return Math.abs(Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2)));
 };
