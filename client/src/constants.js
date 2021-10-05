d0ad601c99a88942ce30929439398f6e2284b39a
GRZ4NA <grzanatv@gmail.com>
2021-10-05 17:12:20 +0200

    added APP_NAME

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,8 @@
 import { createContext } from "react";
 import { APIClient } from "./utils/APIClient";
-require('dotenv').config();
+require("dotenv").config();
+
+export const APP_NAME = "eco-meet";
 export const BACKEND_URL = "http://localhost:5000";
 export const EVENT_CONTEXT = createContext();
 export const API_CLIENT = new APIClient(process.env.DEPLOY ? "" : BACKEND_URL);
