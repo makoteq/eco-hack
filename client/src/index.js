365da661b94c3dcda1b482e4e864b1a4640501e7
GRZ4NA <grzanatv@gmail.com>
2021-10-06 22:08:54 +0200

    added fetchSession (logoff doesn't work)

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import { App } from "./App.js";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { API_CLIENT } from "./constants";
+import { API_CLIENT, LOGIN_MANAGER } from "./constants";
 
 (async () => {
     await API_CLIENT.fetchEvents();
+    await LOGIN_MANAGER.fetchSession();
 
     ReactDOM.render(
         <React.StrictMode>
