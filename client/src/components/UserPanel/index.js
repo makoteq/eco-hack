95c47f59f2d383cf500074de1572b5bafdeddd2b
GRZ4NA <grzanatv@gmail.com>
2021-10-07 19:48:56 +0200

    added dashboard button

--- client/src/components/UserPanel/index.js
+++ client/src/components/UserPanel/index.js
@@ -1,6 +1,7 @@
 import styles from "./index.module.scss";
 import { LOGIN_MANAGER } from "../../constants";
 import { BIcon } from "../BIcon";
+import { Stack } from "react-bootstrap";
 
 export const UserPanel = (props) => {
     if (!LOGIN_MANAGER.state) props.close();
@@ -12,15 +13,25 @@ export const UserPanel = (props) => {
             </button>
             <h3>Zalogowano jako</h3>
             <h1>{LOGIN_MANAGER.state.email ?? "Nieznany użytkownik"}</h1>
-            <button
-                onClick={async () => {
-                    await LOGIN_MANAGER.logout();
-                    props.close();
-                }}
-                className={styles.logoutBtn}
-            >
-                <BIcon icon="box-arrow-right" /> Wyloguj się
-            </button>
+            <Stack gap={2}>
+                <button
+                    onClick={async () => {
+                        await LOGIN_MANAGER.logout();
+                        props.close();
+                    }}
+                    className={styles.logoutBtn}
+                >
+                    <BIcon icon="box-arrow-right" /> Wyloguj się
+                </button>
+                <button
+                    onClick={() => {
+                        props.history?.push("/dashboard");
+                        props.close();
+                    }}
+                >
+                    Dashboard
+                </button>
+            </Stack>
         </div>
     );
 };
