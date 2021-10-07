d6fb190c680a67fe581ac6df537b10f4f6398fb3
GRZ4NA <grzanatv@gmail.com>
2021-10-07 19:25:11 +0200

    major code refactor

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -5,61 +5,18 @@ import { EventPreview } from "../../components/EventPreview";
 import { container } from "../../global.module.scss";
 import styles from "./index.module.scss";
 import { BIcon } from "../../components/BIcon";
-// import HoldingPlant from "../../components/Pop-up/AnimatedHand";
-import { API_CLIENT, LOGIN_MANAGER } from "../../constants";
 import { calculateDistance } from "../../utils/map/calculateDistance";
 import { useHistory } from "react-router";
 import { spawnPopup } from "../../utils/popups/spawnPopup";
 import { spawnError } from "../../utils/popups/spawnError";
 import { getUserPos } from "../../utils/getUserPos";
+import { useLoginState } from "../../utils/useLoginState";
+import { useDbEvents } from "../../utils/useDbEvents";
 
 export const Home = () => {
-    useTitle();
-    const [list, updateList] = useState(
-        API_CLIENT.events.map((e, i) => {
-            return (
-                <EventPreview data={{ name: e.name, type: e.type, lon: e.lon, lat: e.lat, createdTime: e.created?.time, time: e.time, id: e._id, address: e.address }} key={i} />
-            );
-        })
-    );
-    const sortDropdown = useRef(null);
-    const history = useHistory();
-    const sortingFn = async (e) => {
-        const [type, direction] = e.target.value.split(":");
-        let userPos;
-        if (type === "distance") {
-            const permissions = await window.navigator.permissions.query({ name: "geolocation" });
-            if (permissions.state === "granted") {
-                userPos = await getUserPos().catch(async () => {
-                    await spawnError("Wystąpił nieoczekiwany błąd podczas próby dostępu do usługi geolokalizacji");
-                    return null;
-                });
-            } else if (permissions.state === "prompt") {
-                let closeFn;
-                spawnPopup((c) => {
-                    closeFn = c;
-                    return (
-                        <div className={styles.popupContainer}>
-                            <BIcon color="rgb(0,0,100)" icon="info-circle-fill" size={"120px"} />
-                            <h3 style={{ textAlign: "center" }}>Oczekiwanie na przyznanie uprawnień przez użytkownika</h3>
-                        </div>
-                    );
-                });
-                userPos = await getUserPos().catch(async () => {
-                    await spawnError("Nie udało się uzyskać lokalizacji urządzenia lub nastąpiła odmowa dostępu");
-                    return null;
-                });
-                closeFn();
-            } else if (permissions.state === "denied") {
-                await spawnError("Odmówiono aplikacji dostępu do lokalizacji urządzenia");
-            }
-            if (!userPos) {
-                sortDropdown.current.value = "event-date:ascending";
-                window.localStorage.setItem("sorting", "event-date:ascending");
-                return;
-            }
-        }
-        const newOrder = API_CLIENT.events.sort((a, b) => {
+    const sortingFn = (userPos, customMode) => {
+        return (a, b) => {
+            const [type, direction] = customMode ? customMode.split(":") : list.sortingMode.split(":");
             switch (type) {
                 case "event-date":
                     return direction === "ascending" ? a.time - b.time : b.time - a.time;
@@ -68,7 +25,7 @@ export const Home = () => {
                 case "alphabetic":
                     return direction === "ascending" ? (a.name > b.name ? -1 : 1) : a.name > b.name ? 1 : -1;
                 case "distance":
-                    if (userPos !== [null, null]) {
+                    if (userPos) {
                         const aDistance = a.lon && a.lat ? calculateDistance(userPos, [a.lon, a.lat]) : -1;
                         const bDistance = b.lon && b.lat ? calculateDistance(userPos, [b.lon, b.lat]) : -1;
                         return direction === "ascending" ? aDistance - bDistance : bDistance - aDistance;
@@ -76,47 +33,118 @@ export const Home = () => {
                 default:
                     return 0;
             }
-        });
-        updateList(
-            newOrder.map((e, i) => {
+        };
+    };
+    const getPos = async () => {
+        let userPos;
+        const permissions = await window.navigator.permissions.query({ name: "geolocation" });
+        if (permissions.state === "granted") {
+            userPos = await getUserPos().catch(async () => {
+                await spawnError("Wystąpił nieoczekiwany błąd podczas próby dostępu do usługi geolokalizacji");
+                return null;
+            });
+        } else if (permissions.state === "prompt") {
+            let closeFn;
+            spawnPopup((c) => {
+                closeFn = c;
+                return (
+                    <div className={styles.popupContainer}>
+                        <BIcon color="rgb(0,0,100)" icon="info-circle-fill" size={"120px"} />
+                        <h3 style={{ textAlign: "center" }}>Oczekiwanie na przyznanie uprawnień przez użytkownika</h3>
+                    </div>
+                );
+            });
+            userPos = await getUserPos().catch(async () => {
+                await spawnError("Nie udało się uzyskać lokalizacji urządzenia lub nastąpiła odmowa dostępu");
+                return null;
+            });
+            closeFn();
+        } else if (permissions.state === "denied") {
+            await spawnError("Odmówiono aplikacji dostępu do lokalizacji urządzenia");
+        }
+        return userPos ?? null;
+    };
+
+    const events = useDbEvents(async (list) => {
+        updateList({
+            sortingMode: list.sortingMode,
+            elements: list.sort(sortingFn(list.sortingMode.split(":")[0] === "distance" ? await getPos() : null)).map((e, i) => {
                 return (
                     <EventPreview
                         data={{ name: e.name, type: e.type, lon: e.lon, lat: e.lat, createdTime: e.created?.time, time: e.time, id: e._id, address: e.address }}
                         key={i}
                     />
                 );
-            })
-        );
-        window.localStorage.setItem("sorting", `${type}:${direction}`);
-    };
+            }),
+        });
+    });
+    const [list, updateList] = useState({
+        sortingMode: window.localStorage.getItem("sorting") ?? "event-date:ascending",
+        elements: [],
+    });
+    const history = useHistory();
+    const sortDropdown = useRef(null);
+    const createEventButton = useRef(null);
 
+    useTitle();
+    useLoginState((state) => {
+        if (createEventButton.current) createEventButton.current.disabled = state === null;
+    });
     useEffect(() => {
-        const apiClientCallback = (list) => {
-            updateList(
-                list.map((e, i) => {
+        window.localStorage.setItem("sorting", list.sortingMode);
+    }, [list.sortingMode]);
+    useEffect(() => {
+        sortDropdown.current.addEventListener("input", async (e) => {
+            const [type] = e.target.value.split(":");
+            const pos = type === "distance" ? await getPos() : null;
+            if (type === "distance" && !pos) {
+                sortDropdown.current.value = list.sortingMode;
+                window.localStorage.setItem("sorting", list.sortingMode);
+                return;
+            }
+            updateList({
+                sortingMode: e.target.value,
+                elements: events.sort(sortingFn(pos, e.target.value)).map((e, i) => {
                     return (
                         <EventPreview
                             data={{ name: e.name, type: e.type, lon: e.lon, lat: e.lat, createdTime: e.created?.time, time: e.time, id: e._id, address: e.address }}
                             key={i}
                         />
                     );
-                })
-            );
-        };
-        API_CLIENT.on("EVENT_RELOAD", apiClientCallback);
-        API_CLIENT.on("EVENT_CREATE", apiClientCallback);
-
-        sortDropdown.current.addEventListener("input", sortingFn);
-        const cachedValue = window.localStorage.getItem("sorting");
-        sortingFn(cachedValue ? { target: { value: cachedValue } } : { target: sortDropdown.current });
-        if (cachedValue) sortDropdown.current.value = cachedValue;
+                }),
+            });
+        });
+        sortDropdown.current.value = list.sortingMode;
+        list.sortingMode.split(":")[0] === "distance"
+            ? getPos().then((pos) => {
+                  updateList({
+                      sortingMode: list.sortingMode,
+                      elements: events.sort(sortingFn(pos)).map((e, i) => {
+                          return (
+                              <EventPreview
+                                  data={{ name: e.name, type: e.type, lon: e.lon, lat: e.lat, createdTime: e.created?.time, time: e.time, id: e._id, address: e.address }}
+                                  key={i}
+                              />
+                          );
+                      }),
+                  });
+              })
+            : updateList({
+                  sortingMode: list.sortingMode,
+                  elements: events.sort(sortingFn(null)).map((e, i) => {
+                      return (
+                          <EventPreview
+                              data={{ name: e.name, type: e.type, lon: e.lon, lat: e.lat, createdTime: e.created?.time, time: e.time, id: e._id, address: e.address }}
+                              key={i}
+                          />
+                      );
+                  }),
+              });
+        // eslint-disable-next-line
     }, []);
 
     return (
         <div className={container}>
-            {/* <div className={styles.hand}>
-                <HoldingPlant />
-            </div> */}
             <Stack gap={1}>
                 <Stack gap={2} direction={"horizontal"} className={styles.sortDropdown}>
                     <p className={styles.label}>
@@ -133,7 +161,7 @@ export const Home = () => {
                         <option value="distance:descending">Odległość: najdalej</option>
                     </select>
                     <button
-                        disabled={!LOGIN_MANAGER.state}
+                        ref={createEventButton}
                         onClick={() => {
                             history.push("/create");
                         }}
@@ -143,7 +171,7 @@ export const Home = () => {
                         Utwórz wydarzenie
                     </button>
                 </Stack>
-                {list}
+                {list.elements}
             </Stack>
         </div>
     );
