659d6b0e7d88d0ab525d415c5fac971cbc3d3d7f
Maciej Kotowski <makoteq@gmail.com>
2021-10-07 19:44:30 +0200

    fix heroku

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,5 @@
 import { APIClient } from "./utils/APIClient";
 import { LoginManager } from "./utils/LoginManager";
-require("dotenv").config();
 
 export const APP_NAME = "eco-meet";
 export const BACKEND_URL = "http://localhost:5000";
