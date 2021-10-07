2cbedb897fe433ce9198a7dce29065eb234602eb
GRZ4NA <grzanatv@gmail.com>
2021-10-07 19:24:17 +0200

    fixed hooks

--- client/src/utils/useDbEvents.js
+++ client/src/utils/useDbEvents.js
@@ -1,18 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { API_CLIENT } from "../constants";
 
 export const useDbEvents = (callback) => {
     if (typeof callback !== "function") throw new TypeError("Invalid callback");
     const [componentState, updateComponentState] = useState(API_CLIENT.events);
     useEffect(() => {
-        API_CLIENT.on("EVENT_RELOAD", (events, data) => {
+        const listenerFn = (events, data) => {
             callback(events, data);
             updateComponentState(events);
-        });
-        API_CLIENT.on("EVENT_RELOAD", (events, data) => {
-            callback(events, data);
-            updateComponentState(events);
-        });
+        };
+        API_CLIENT.on("EVENT_RELOAD", listenerFn);
+        API_CLIENT.on("EVENT_CREATE", listenerFn);
+        return () => {
+            API_CLIENT.removeListener("EVENT_RELOAD", listenerFn);
+            API_CLIENT.removeListener("EVENT_CREATE", listenerFn);
+        };
+        // eslint-disable-next-line
     }, []);
     return componentState;
 };
--- client/src/utils/useLoginState.js
+++ client/src/utils/useLoginState.js
@@ -5,10 +5,14 @@ export const useLoginState = (callback) => {
     if (callback && typeof callback !== "function") throw new TypeError("Invalid callback");
     const [componentState, updateComponentState] = useState(LOGIN_MANAGER.state);
     useEffect(() => {
-        LOGIN_MANAGER.on("STATE_CHANGE", (state) => {
+        const listenerFn = (state) => {
             callback && callback(state);
             updateComponentState(state);
-        });
-    }, []);
+        };
+        LOGIN_MANAGER.on("STATE_CHANGE", listenerFn);
+        return () => {
+            LOGIN_MANAGER.removeListener("STATE_CHANGE", listenerFn);
+        };
+    }, [callback]);
     return componentState;
 };
