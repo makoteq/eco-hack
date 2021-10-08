fe1f29ea394c2f9a312bed5f5b2983623d1937e3
GRZ4NA <grzanatv@gmail.com>
2021-10-08 20:10:27 +0200

    added OSM

--- client/src/pages/Credits/index.js
+++ client/src/pages/Credits/index.js
@@ -6,44 +6,48 @@ import { ReactComponent as Person2 } from "../../assets/person2.svg";
 import "./index.scss";
 import { container } from "../../global.module.scss";
 export const Credits = () => {
-  return (
-    //Icons
-    <div className={container}>
-    <div className="credits">
-      <div className="credits__team">
-        <h6>THE ECO-MEET TEAM</h6>
-        <br />
-        <Person />{" "}
-        <a href="https://github.com/GRZ4NA" title="GRZ4NA">
-          GRZ4NA
-        </a>
-        <br />
-        <br />
-        <Person1 />{" "}
-        <a href="https://github.com/makoteq" title="makoteq">
-          makoteq
-        </a>
-        <br />
-        <br />
-        <Person2 />{" "}
-        <a href="https://github.com/drgoodcat" title="drgoodcat">
-          drgoodcat
-        </a>
-      </div>
+    return (
+        //Icons
+        <div className={container}>
+            <div className="credits">
+                <div className="credits__team">
+                    <h6>THE ECO-MEET TEAM</h6>
+                    <br />
+                    <Person />{" "}
+                    <a href="https://github.com/GRZ4NA" title="GRZ4NA">
+                        GRZ4NA
+                    </a>
+                    <br />
+                    <br />
+                    <Person1 />{" "}
+                    <a href="https://github.com/makoteq" title="makoteq">
+                        makoteq
+                    </a>
+                    <br />
+                    <br />
+                    <Person2 />{" "}
+                    <a href="https://github.com/drgoodcat" title="drgoodcat">
+                        drgoodcat
+                    </a>
+                </div>
 
-      <div className="credits__icon">
-        Icons made by{" "}
-        <a
-          href="https://www.flaticon.com/authors/pixel-perfect"
-          title="Pixel perfect"
-        >
-          Pixel perfect
-        </a>{" "}
-        from{" "}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-        </a>
-      </div>
-    </div>
-    </div>);
+                <div className="credits__icon">
+                    Icons made by{" "}
+                    <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">
+                        Pixel perfect
+                    </a>{" "}
+                    from{" "}
+                    <a href="https://www.flaticon.com/" title="Flaticon">
+                        www.flaticon.com
+                    </a>
+                </div>
+                <div className="credits__icon">
+                    Map data provided by{" "}
+                    <a href="https://www.openstreetmap.org/" aria-label="OpenStreetMap" rel="noreferrer">
+                        OpenStreetMap
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
 };
